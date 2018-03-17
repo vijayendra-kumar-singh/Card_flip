708e4125b8404f5e8a26df9fe3b1697d73afe9e9
Vijayendra <vijayendra.kumar2015@vit.ac.in>
2018-03-18 01:59:34 +0530

    flipView

--- app/src/main/java/com/example/prize/FullscreenActivity.java
+++ app/src/main/java/com/example/prize/FullscreenActivity.java
@@ -207,7 +207,6 @@ public class FullscreenActivity extends AppCompatActivity {
 //        mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
         mHideHandler.removeCallbacks(mShowPart2Runnable);
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
